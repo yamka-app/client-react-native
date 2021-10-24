af5acff5027b9e33ebcd165b4e3e6e8e8fdec0dd
portasynthinca3 <portasynthinca3@gmail.com>
2021-10-24 13:45:15 +0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>I &lt;3 React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 25,
+    textShadowColor: "#f00",
+    textShadowRadius: 1,
+    textShadowOffset: {width: 2, height: 2}
+  }
+});
